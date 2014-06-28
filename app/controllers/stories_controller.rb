6446ceaa35791d3eaecd8729a17d5565ba2beb68
Blair Anderson <blair81@gmail.com>
2014-06-27 23:38:20 -0600

    begin the layout

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -2,6 +2,8 @@ class StoriesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @stories = current_user.feeds.map(&:stories).flatten.sort_by(&:published)
+    @stories = current_user.feeds.map(&:stories).flatten.sort_by(&:published).group_by do |g|
+      "#{g.published.day} #{g.published.month}"
+    end
   end
 end
