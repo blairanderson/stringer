a4b5917a37b6930765c5fa07b8b67eace358f591
Blair Anderson <Blair@quickleft.com>
2014-07-06 19:42:31 -0600

    refactor content_nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def content_nav_link_to(name ,path)
+    options = {}
+    options[:class] = 'active' if current_page?(path)
+    content_tag(:li, options) { link_to name, path }
+  end
 end
