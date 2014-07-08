7b17850f67ecaff86b2bbdfead79ec1b1b68eeb7
Blair Anderson <Blair@quickleft.com>
2014-07-08 09:08:55 -0600

    add schedule_times update ability

--- app/controllers/schedule_times_controller.rb
+++ app/controllers/schedule_times_controller.rb
@@ -1,19 +1,40 @@
 class ScheduleTimesController < AccountsController
   before_action :set_schedule
+  before_action :set_time, only: [:update, :destroy]
 
   def create
     @schedule.schedule_times.create(time: Time.now)
-    redirect_to :back
+    schedule_times_response
+  end
+
+  def update
+    if @time.update(time_params)
+      schedule_times_response
+    else
+      flash[:error] = "BROKEN"
+      schedule_times_response
+    end
   end
 
   def destroy
-    time = @schedule.schedule_times.find(params[:id])
-    time.destroy
-    redirect_to :back
+    @time.destroy
+    schedule_times_response
+  end
+
+  def schedule_times_response
+    redirect_to schedules_path
   end
 
   private
   def set_schedule
     @schedule = current_user.schedules.find(params[:schedule_id])
+  end
+
+  def set_time
+    @time = @schedule.schedule_times.find(params[:id])
+  end
+
+  def time_params
+    params.require(:schedule_time).permit(:time)
   end
 end
