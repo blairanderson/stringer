bed8dbc414195d4e1d3d8f9004866a0127f2e4e0
Blair Anderson <Blair@quickleft.com>
2014-07-08 19:18:40 -0600

    add flashes to the layout

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,7 +1,4 @@
 class StoriesController < AccountsController
-  # TODO: figure out a good way to include the .user_stories with the stories
-  # Would like to know if a user has read a given story. or favorited a given story.
-
   def index
     @stories = current_user.grouped_stories
   end
