d77281ee66efae225ef8782ed2569e05090a7a9d
Blair Anderson <blair81@gmail.com>
2014-07-08 22:22:47 -0600

    helper method for flashes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,15 @@
 module ApplicationHelper
-  def content_nav_link_to(name ,path)
+  def content_nav_link_to(name, path)
     options = {}
     options[:class] = 'active' if current_page?(path)
     content_tag(:li, options) { link_to name, path }
+  end
+
+  def flash_to_class(flash)
+    # this helps you map flash messages to CSS classes.
+    {
+        "notice" => "info"
+
+    }[flash]
   end
 end
