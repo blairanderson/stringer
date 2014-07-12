2496fd89aaae377dae7ac0392653becebb7d4422
Blair Anderson <blair81@gmail.com>
2014-07-12 01:04:24 -0600

    Add Zenpen to the story form

--- app/assets/javascripts/stories.js
+++ app/assets/javascripts/stories.js
@@ -8,3 +8,42 @@ StoriesController.prototype.index = function () {
         $(this).removeData('modal');
     });
 };
+
+StoriesController.prototype.edit = function () {
+    debugger
+    var _message_change = function _message_change() {
+        var $rawText = $(this).val();
+        console.log('raw text', $rawText);
+        var $autoLinked = twttr.txt.autoLink($rawText);
+        console.log($autoLinked);
+    };
+    var messageChange = _.debounce(_message_change, 1000);
+
+    var message_key_up = function messageKeyUp() {
+        var $text = $(this),
+            $form = $text.parents('form'),
+            $targetLabel = $form.find('button.length'),
+            $actions = $form.find('.actions'),
+            length = twttr.txt.getTweetLength($text.val()),
+            lengthRemaining = 140 - length;
+
+        $targetLabel.text(lengthRemaining);
+        $actions.removeClass('danger disabled');
+        if (lengthRemaining < 20) {
+            $actions.addClass('danger')
+        }
+
+        if (lengthRemaining < 0) {
+            $actions.addClass('disabled')
+        }
+    };
+    var messageKeyUp = _.throttle(message_key_up, 100);
+
+    $(document).on('keyup', "article.content", messageKeyUp);
+
+};
+
+
+
+
+
