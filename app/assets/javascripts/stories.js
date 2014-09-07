eed7ce3b9c2a4e7fe716c2f360f77e8421e80b91
Blair Anderson <blair81@gmail.com>
2014-09-07 14:30:28 -0700

    code style

--- app/assets/javascripts/stories.js
+++ app/assets/javascripts/stories.js
@@ -4,15 +4,15 @@
 var StoriesController = Paloma.controller('Stories');
 
 StoriesController.prototype.index = function () {
-    $('.truncate').hide();
-    $('.toggle-panel-body').on("click", function (e) {
-        e.preventDefault();
-        var target = $(this).attr("href");
-        $("[data-href='" + target + "']").slideToggle();
-    });
+  $('.truncate').hide();
+  $('.toggle-panel-body').on("click", function (e) {
+    e.preventDefault();
+    var target = $(this).attr("href");
+    $("[data-href='" + target + "']").slideToggle();
+  });
 };
 
 StoriesController.prototype.edit = function () {
-    var handler = new MessageForm();
-    handler.init()
+  var handler = new MessageForm();
+  handler.init()
 };
