7a803534028bd0e9cbee370494536996b279b94b
Blair Anderson <blair81@gmail.com>
2014-09-13 22:17:38 -0700

    remove un-used method

--- app/models/user.rb
+++ app/models/user.rb
@@ -48,7 +48,7 @@ class User < ActiveRecord::Base
             email: email ? email : "#{TEMP_EMAIL_PREFIX}-#{auth.uid}-#{auth.provider}.com",
             password: Devise.friendly_token[0, 20]
         )
-        user.skip_confirmation!
+        # user.skip_confirmation! TODO:confirmable
         user.save!
       end
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -22,3 +22,12 @@ end
 
 FetchFeeds.new(b.feeds).fetch_all
 FetchFeeds.new(e.feeds).fetch_all
+
+messages = [
+    "If you're reading this comment, then you'll probably be affected by this illness - http://www.newrepublic.com/article/119265/alzheimers-disease-statistics-show-illness-will-define-our-times/",
+    "The new bionics that let us run, climb and dance http://www.ted.com/talks/hugh_herr_the_new_bionics_that_let_us_run_climb_and_dance",
+    "some beautiful daytime disco - https://soundcloud.com/poolside/chillin-out-poolside-edit",
+    "another rad disco mi - https://soundcloud.com/poolside/poolside-stir-it-up-mixtape"
+].map { |content| {content: content} }
+
+b.messages.create(messages)
