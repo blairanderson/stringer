40288572dba66d0c447630f07a04eaada5b520a1
Blair Anderson <blair81@gmail.com>
2014-09-21 22:45:11 -0700

    remove un-used

--- app/jobs/messenger.rb
+++ app/jobs/messenger.rb
@@ -1,17 +1,9 @@
 class Messenger
-  def initialize(deps)
-    @user = deps.user
-    raise('missing user') if @user.nil?
-    @service = deps.service
-    raise('missing service') if @service.nil?
-    @message = deps.message
-    raise('missing message') if @message.nil?
+  def initialize(user_id)
+    @user_id = user_id
+    raise('missing user') if @user_id
   end
 
   def perform
-    service = @user.services.where(name: @service).first
-    if service
-      service.client.update(message)
-    end
   end
 end
