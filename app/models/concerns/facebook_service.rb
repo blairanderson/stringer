4f5c197ceb2f4d1fef1414068e1e5fab65b253af
Blair Anderson <blair81@gmail.com>
2014-09-22 22:41:53 -0700

    implement twitter/facebook service

--- app/models/concerns/facebook_service.rb
+++ app/models/concerns/facebook_service.rb
@@ -1,2 +1,20 @@
 class FacebookService
+  def initialize(auth)
+    #TODO if auth.expires => true
+    #if auth.expires_at is in the past
+    #attempt to renew it and continue
+    @client = FbGraph::User.me(auth.token)
+  end
+
+  def update(message)
+    if message
+      @client.feed!(message: message)
+      # TODO: link: 'https://github.com/nov/fb_graph'
+      # TODO: name: 'FbGraph'
+      # TODO: picture: 'https://graph.facebook.com/matake/picture'
+      # TODO: description: 'A Ruby wrapper for Facebook Graph API'
+    end
+  end
 end
+
+
--- app/models/concerns/twitter_service.rb
+++ app/models/concerns/twitter_service.rb
@@ -1,10 +1,16 @@
 class TwitterService
   def initialize(auth)
-    @token = auth.token
-    @secret = auth.secret
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key         = ENV["TWITTER_API_KEY"]
+      config.consumer_secret      = ENV["TWITTER_API_SECRET"]
+      config.access_token         = auth.token
+      config.access_token_secret  = auth.secret
+    end
   end
 
   def update(message)
-    TwitterClient.new()
+    if message
+      @client.update(message)
+    end
   end
 end
