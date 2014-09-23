d3bf80e5675ed50625a0579496f8ce6afe1e4a56
Blair Anderson <blair81@gmail.com>
2014-09-22 22:43:48 -0700

    remove some junk :)

--- app/jobs/messenger.rb
+++ app/jobs/messenger.rb
@@ -1,14 +1,4 @@
 class Messenger
-
-  STUFF = {
-      user_id: 2,
-      service: "twitter",
-      message: {
-          'id' =>7,
-          'user_id' =>2,
-          'content' =>"yolo this is the last"
-      }
-  }
   def initialize(deps)
     @user_id = deps.user_id
     raise('missing user') unless @user_id
